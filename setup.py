703a4ece1e9b612e6c01fd3f9cf666724fb13095
Kostas Perifanos <kostas.perifanos@gmail.com>
2024-09-09 11:48:09 +0300

    update version minor number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="hydrogap-ai",
-    version="0.2.0",
+    version="0.3.1",
     author="Konstantinos Perifanos",
     author_email="kostas.perifanos@gmail.com",
     description="HydroGAP-AI: Hydro-Gap Artificial Intelligence",
